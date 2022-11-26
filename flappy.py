15607069a225931ce1ce58b37ce5b06067b533f1
RiskingGhost <109953230+RiskingGhost@users.noreply.github.com>
2022-11-26 22:25:59 +0700

    flappy

--- flappy.py
+++ flappy.py
@@ -2,11 +2,11 @@ import pygame
 from pygame.locals import *
 import random
 
-#This is Aum"
-#This is Flame"
+"This is Aum"
+"This is Flame"
 #hello, my name is Atom
 #i am mart
-#Mile
+#Yes!
 pygame.init()
 
 clock = pygame.time.Clock()
@@ -29,20 +29,18 @@ ground_scroll = 0
 scroll_speed = 4
 flying = False
 game_over = False
-pipe_gap = 200
-immortal = 0
-
+pipe_gap =200
 pipe_frequency = 1500 #milliseconds
 last_pipe = pygame.time.get_ticks() - pipe_frequency
 score = 0
 pass_pipe = False
-check = False
+boss = False
+
 
 #load images
 bg = pygame.image.load('img/bg.png')
 ground_img = pygame.image.load('img/ground.png')
 button_img = pygame.image.load('img/restart.png')
-heart_img = pygame.image.load('img/heart.png')
 
 
 #function for outputting text onto the screen
@@ -54,19 +52,17 @@ def reset_game():
 	pipe_group.empty()
 	flappy.rect.x = 100
 	flappy.rect.y = int(screen_height / 2)
-	flappy.heart = 3
 	score = 0
 	return score
 
 
 class Bird(pygame.sprite.Sprite):
 
-	def __init__(self, x, y, heart):
+	def __init__(self, x, y):
 		pygame.sprite.Sprite.__init__(self)
 		self.images = []
 		self.index = 0
 		self.counter = 0
-		self.heart = heart
 		for num in range (1, 4):
 			img = pygame.image.load(f"img/bird{num}.png")
 			self.images.append(img)
@@ -158,14 +154,26 @@ class Button():
 
 		return action
 
+class Bullet(pygame.sprite.Sprite):
+	def __init__(self, x, y):
+		pygame.sprite.Sprite.__init__(self)
+		self.image = pygame.image.load('img/rock.png')
+		self.rect = self.image.get_rect()
+		self.rect.center = [x, y]
+	
+	def update(self):
+		self.rect.x += 10
+		if self.rect.left < 0:
+			self.kill()
 
 
 pipe_group = pygame.sprite.Group()
 bird_group = pygame.sprite.Group()
+bullet_group = pygame.sprite.Group()
 
-flappy = Bird(100, int(screen_height / 2), 3)
-
+flappy = Bird(100, int(screen_height / 2))
 bird_group.add(flappy)
+
 
 #create restart button instance
 button = Button(screen_width // 2 - 50, screen_height // 2 - 100, button_img)
@@ -180,14 +188,9 @@ while run:
 	screen.blit(bg, (0,0))
 
 	pipe_group.draw(screen)
-	# immortal
-	if immortal%2 == 0 or game_over == True:
-		bird_group.draw(screen)
+	bird_group.draw(screen)
 	bird_group.update()
-
-	#draw heart
-	for x in range(flappy.heart):
-		screen.blit(heart_img, (10 + (x * 30), 10))
+	bullet_group.draw(screen)
 
 	#draw and scroll the ground
 	screen.blit(ground_img, (ground_scroll, 768))
@@ -204,22 +207,17 @@ while run:
 				pass_pipe = False
 	draw_text(str(score), font, white, int(screen_width / 2), 20)
 
+
 	#look for collision
-	# cooldown for immortal 
-	if immortal > 0:
-		immortal -= 1
-	elif pygame.sprite.groupcollide(bird_group, pipe_group, False, False):
-			flappy.heart -= 1
-			immortal = 35 
-	if flappy.rect.top < 0:
-		flappy.heart = 0
+	if pygame.sprite.groupcollide(bird_group, pipe_group, False, False) or flappy.rect.top < 0:
+		game_over = True
 	#once the bird has hit the ground it's game over and no longer flying
 	if flappy.rect.bottom >= 768:
-		flappy.heart = 0
+		game_over = True
 		flying = False
 
 
-	if flying == True and game_over == False:
+	if flying == True and game_over == False and boss == False:
 		#generate new pipes
 		time_now = pygame.time.get_ticks()
 		if time_now - last_pipe > pipe_frequency:
@@ -229,24 +227,32 @@ while run:
 			pipe_group.add(btm_pipe)
 			pipe_group.add(top_pipe)
 			last_pipe = time_now
-
+		
 		pipe_group.update()
 
 		ground_scroll -= scroll_speed
 		if abs(ground_scroll) > 35:
 			ground_scroll = 0
-	
+	if flying == True and game_over == False and boss == True:
+		#generate new pipes
+		time_now = pygame.time.get_ticks()
+		if time_now - last_pipe > pipe_frequency // 8:
+			shoot = Bullet(bird_group.sprites()[0].rect.centerx, \
+			bird_group.sprites()[0].rect.centery)
+			bullet_group.add(shoot)
+			last_pipe = time_now
 
-	# check for game over and reset
-	# check heart
-	if flappy.heart == 0:
-		game_over = True
+		bullet_group.update()
+
+		ground_scroll -= scroll_speed
+		if abs(ground_scroll) > 35:
+			ground_scroll = 0
+
+	#check for game over and reset
 	if game_over == True:
 		if button.draw():
 			game_over = False
 			score = reset_game()
-			check = True
-			immortal = 0
 
 
 	for event in pygame.event.get():
